c5e8a5d4c5d3997f21ca80d69548f90eb9d70417
horatiu <horatiu.cod@gmail.com>
2025-10-13 08:23:47 +0300

    added json

--- GimuGuardBot/Program.cs
+++ GimuGuardBot/Program.cs
@@ -1,10 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
 using Telegram.Bot;
-
+ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+IConfiguration configuration = configurationBuilder.AddUserSecrets<Program>().Build();
+string botToken = configuration.GetSection("GuardBot")["Token"]; 
 Console.WriteLine("Hello, World!");
 
 
-var botClient = new TelegramBotClient("8475907726:AAGi7wK81pzumjE2osYPcCsVMbKlHN4qrn4");
+var botClient = new TelegramBotClient(botToken);
 
 var me = await botClient.GetMe();
 Console.WriteLine($"Hello, World! I am bot {me.Id} and my name is {me.FirstName}.");
